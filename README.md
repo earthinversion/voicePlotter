996dad25261d69657a43b15c298c0c6933e0d59d
earthinversion <utpalkumar50@gmail.com>
2021-01-24 14:53:02 +0800

    update

---
+++ README.md
@@ -0,0 +1,25 @@
+# Voice Plotter Application using PyQt5 and QT Designer
+
+<p align="center">
+  <img width="80%" src="docs/screenshot.jpg">
+</p>
+
+## Installation & Run
+
+### Using Anaconda
+
+```
+conda create -n voiceplotter
+conda activate voiceplotter
+pip install PyQt5 matplotlib sounddevice
+python voicePlotter.py
+```
+
+### Using venv (in Python 3 only)
+
+```
+python3 -m venv voiceplotter
+source voiceplotter/bin/activate
+pip install PyQt5 matplotlib sounddevice
+python voicePlotter.py
+```
--- gui.py
+++ gui.py
@@ -1,8 +1,3 @@
-# Welcome to PyShine
-# This is part 10 of the PyQt5 learning series
-# Based on parameters, the GUI will plot live voice data using Matplotlib in PyQt5
-# We will use Qthreads to run the audio stream data.
-
 import sys
 import matplotlib
 
@@ -18,7 +13,7 @@ from PyQt5 import QtCore, QtWidgets, QtGui
 from PyQt5 import uic
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtMultimedia import QAudioDeviceInfo, QAudio, QCameraInfo
-import threading
+import time
 
 input_audio_deviceInfos = QAudioDeviceInfo.availableDevices(QAudio.AudioInput)
 
@@ -26,7 +21,9 @@ input_audio_deviceInfos = QAudioDeviceInfo.availableDevices(QAudio.AudioInput)
 class MplCanvas(FigureCanvas):
     def __init__(self, parent=None, width=5, height=4, dpi=100):
         fig = Figure(figsize=(width, height), dpi=dpi)
+        fig.patch.set_facecolor("#00B28C")
         self.axes = fig.add_subplot(111)
+
         super(MplCanvas, self).__init__(fig)
         fig.tight_layout()
 
@@ -37,8 +34,12 @@ class main(QtWidgets.QMainWindow):
         self.ui = uic.loadUi("main.ui", self)
         self.resize(888, 600)
         icon = QtGui.QIcon()
-        # icon.addPixmap(QtGui.QPixmap("PyShine.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        # icon.addPixmap(
+        #     QtGui.QPixmap("PyShine.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off
+        # )
         self.setWindowIcon(icon)
+        self.threadpool = QtCore.QThreadPool()
+        self.threadpool.setMaxThreadCount(1)
         self.devices_list = []
         for device in input_audio_deviceInfos:
             self.devices_list.append(device.deviceName())
@@ -48,6 +49,7 @@ class main(QtWidgets.QMainWindow):
         self.comboBox.setCurrentIndex(0)
 
         self.canvas = MplCanvas(self, width=5, height=4, dpi=100)
+        self.canvas.axes.set_facecolor("#D5F9FF")
         self.ui.gridLayout_4.addWidget(self.canvas, 2, 1, 1, 1)
         self.reference_plot = None
         self.q = queue.Queue(maxsize=20)
@@ -57,6 +59,8 @@ class main(QtWidgets.QMainWindow):
         self.downsample = 1
         self.channels = [1]
         self.interval = 30
+        self.yrangeMinVal = -0.5
+        self.yrangeMaxVal = 0.5
 
         device_info = sd.query_devices(self.device, "input")
         self.samplerate = device_info["default_samplerate"]
@@ -64,21 +68,27 @@ class main(QtWidgets.QMainWindow):
         sd.default.samplerate = self.samplerate
 
         self.plotdata = np.zeros((length, len(self.channels)))
-
-        self.update_plot()
         self.timer = QtCore.QTimer()
         self.timer.setInterval(self.interval)  # msec
         self.timer.timeout.connect(self.update_plot)
         self.timer.start()
+        self.data = [0]
         self.lineEdit.textChanged["QString"].connect(self.update_window_length)
-        self.lineEdit_2.textChanged["QString"].connect(self.update_sample_rate)
-        self.lineEdit_3.textChanged["QString"].connect(self.update_down_sample)
-        self.lineEdit_4.textChanged["QString"].connect(self.update_interval)
+        self.lineEdit_2.textChanged.connect(self.update_sample_rate)
+        self.spinBox_downsample.valueChanged.connect(self.update_down_sample)  #####
+        self.spinBox_updateInterval.valueChanged.connect(self.update_interval)
+
+        self.doubleSpinBox_yrangemin.valueChanged.connect(self.update_yrange_min)
+        self.doubleSpinBox_yrangemax.valueChanged.connect(self.update_yrange_max)
+
         self.pushButton.clicked.connect(self.start_worker)
-        self.pushButton_exit.clicked.connect(self.sys_exit)
+        self.pushButton_2.clicked.connect(self.stop_worker)
+        self.worker = None
+        self.go_on = False
 
     def getAudio(self):
         try:
+            QtWidgets.QApplication.processEvents()
 
             def audio_callback(indata, frames, time, status):
                 self.q.put(indata[:: self.downsample, [0]])
@@ -90,78 +100,124 @@ class main(QtWidgets.QMainWindow):
                 callback=audio_callback,
             )
             with stream:
-                input()
+
+                while True:
+                    QtWidgets.QApplication.processEvents()
+                    if self.go_on:
+
+                        break
+            self.enable_buttons()
+
         except Exception as e:
-            print("ERROR: ", e)
+            # print("ERROR: ", e)
+            self.stop_worker
+            pass
 
-    def start_worker(self):
-        # worker = Worker(
-        #     self.start_stream,
-        # )
-        # self.threadpool.tryStart(worker)
-        self.threadBackend = threading.Thread(target=self.start_stream, daemon=True)
-        self.threadBackend.start()
-
-    def sys_exit(self):
-        self.timer.stop()
-
-        sys.exit()
-
-    def start_stream(self):
+    def disable_buttons(self):
         self.lineEdit.setEnabled(False)
         self.lineEdit_2.setEnabled(False)
-        self.lineEdit_3.setEnabled(False)
-        self.lineEdit_4.setEnabled(False)
+        self.spinBox_downsample.setEnabled(False)
+        self.spinBox_updateInterval.setEnabled(False)
         self.comboBox.setEnabled(False)
         self.pushButton.setEnabled(False)
+        self.pushButton.setStyleSheet(
+            "QPushButton" "{" "background-color : lightblue;" "}"
+        )
+
+        self.canvas.axes.clear()
+
+    def enable_buttons(self):
+        self.pushButton.setEnabled(True)
+        self.lineEdit.setEnabled(True)
+        self.lineEdit_2.setEnabled(True)
+        self.spinBox_downsample.setEnabled(True)
+        self.spinBox_updateInterval.setEnabled(True)
+        self.comboBox.setEnabled(True)
+
+    def start_worker(self):
+
+        self.disable_buttons()
+
+        self.canvas.axes.clear()
+
+        self.go_on = False
+        self.worker = Worker(
+            self.start_stream,
+        )
+        self.threadpool.start(self.worker)
+        self.reference_plot = None
+        self.timer.setInterval(self.interval)  # msec
+
+    def stop_worker(self):
+
+        self.go_on = True
+        with self.q.mutex:
+            self.q.queue.clear()
+        self.pushButton.setStyleSheet(
+            "QPushButton"
+            "{"
+            "background-color : rgb(92, 186, 102);"
+            "}"
+            "QPushButton"
+            "{"
+            "color : white;"
+            "}"
+        )
+        self.enable_buttons()
+
+    def start_stream(self):
         self.getAudio()
 
     def update_now(self, value):
         self.device = self.devices_list.index(value)
-        print("Device:", self.devices_list.index(value))
 
     def update_window_length(self, value):
         self.window_length = int(value)
         length = int(self.window_length * self.samplerate / (1000 * self.downsample))
         self.plotdata = np.zeros((length, len(self.channels)))
-        self.update_plot()
 
     def update_sample_rate(self, value):
         self.samplerate = int(value)
         sd.default.samplerate = self.samplerate
         length = int(self.window_length * self.samplerate / (1000 * self.downsample))
+        print(self.samplerate, sd.default.samplerate)
         self.plotdata = np.zeros((length, len(self.channels)))
-        self.update_plot()
 
     def update_down_sample(self, value):
         self.downsample = int(value)
         length = int(self.window_length * self.samplerate / (1000 * self.downsample))
         self.plotdata = np.zeros((length, len(self.channels)))
-        self.update_plot()
 
     def update_interval(self, value):
         self.interval = int(value)
-        self.timer.setInterval(self.interval)  # msec
-        self.timer.timeout.connect(self.update_plot)
-        self.timer.start()
+
+    def update_yrange_min(self, minval):
+        self.yrangeMinVal = float(minval)
+
+    def update_yrange_max(self, maxval):
+        self.yrangeMaxVal = float(maxval)
 
     def update_plot(self):
         try:
-            data = [0]
 
-            while True:
+            print("ACTIVE THREADS:", self.threadpool.activeThreadCount(), end=" \r")
+            self.label_18.setText(f"{self.threadpool.activeThreadCount()}")
+            while self.go_on is False:
+                QtWidgets.QApplication.processEvents()
                 try:
-                    data = self.q.get_nowait()
+                    self.data = self.q.get_nowait()
+
                 except queue.Empty:
                     break
-                shift = len(data)
+
+                shift = len(self.data)
                 self.plotdata = np.roll(self.plotdata, -shift, axis=0)
-                self.plotdata[-shift:, :] = data
+                self.plotdata[-shift:, :] = self.data
                 self.ydata = self.plotdata[:]
-                self.canvas.axes.set_facecolor((0, 0, 0))
+                self.canvas.axes.set_facecolor("#D5F9FF")
 
                 if self.reference_plot is None:
-                    plot_refs = self.canvas.axes.plot(self.ydata, color=(0, 1, 0.29))
+                    plot_refs = self.canvas.axes.plot(self.ydata, color="green")
                     self.reference_plot = plot_refs[0]
                 else:
                     self.reference_plot.set_ydata(self.ydata)
@@ -172,16 +228,30 @@ class main(QtWidgets.QMainWindow):
             self.canvas.axes.yaxis.set_major_formatter(
                 ticker.FormatStrFormatter("%0.1f")
             )
-            self.canvas.axes.set_ylim(ymin=-0.5, ymax=0.5)
+            self.canvas.axes.set_ylim(ymin=self.yrangeMinVal, ymax=self.yrangeMaxVal)
+
             self.canvas.draw()
-        except:
+        except Exception as e:
+            print("Error:", e)
             pass
+
+
+class Worker(QtCore.QRunnable):
+    def __init__(self, function, *args, **kwargs):
+        super(Worker, self).__init__()
+        self.function = function
+        self.args = args
+        self.kwargs = kwargs
+
+    @pyqtSlot()
+    def run(self):
+
+        self.function(*self.args, **self.kwargs)
 
 
 app = QtWidgets.QApplication(sys.argv)
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
     mainWindow = main()
     mainWindow.show()
     sys.exit(app.exec_())
